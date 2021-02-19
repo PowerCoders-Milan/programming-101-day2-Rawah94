2401949d2131bd1e6b8183c20a50481117687820
Rawah G <rawah.h@outlook.com>
2021-02-19 12:35:25 +0100

    solved?

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -1,8 +1,23 @@
 //insert your pseudocode below
 /*
+create variable name 
+
+name = rawah
+
+input: say hello rawah
+
+print hello name
 
 here
 
 */
 
 //insert your code below
+var Name, Rawah;
+
+
+'' + String(window.prompt('Hello'));
+
+Name = Rawah;
+
+window.alert('Hello Rawah       ');
